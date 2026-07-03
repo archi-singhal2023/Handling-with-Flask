3debd0f2adbdb7c43a1fe06a5c1d0171a8966c63
Archi <archi.singhal2023@ssipmt.com>
2026-07-03 14:56:27 +0530

    built base template

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template,  request
+
+app = Flask(__name__)
+
+@app.route("/feedback", methods=["GET", "POST"])
+def feedback():
+    if request.method == "POST":
+        name = request.form.get("username")
+        message = request.form.get("message")
+        
+        return render_template("thankyou.html", user=name, message=message)
+    
+    return render_template("feedback.html")
