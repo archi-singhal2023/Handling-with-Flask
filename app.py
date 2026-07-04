0e068b810a18a738a3be83c20b870068fd767cd3
Archi <archi.singhal2023@ssipmt.com>
2026-07-04 18:58:05 +0530

    Used Flash in jinja

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template,  request
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
+app.secret_key = "my-secret-key"
 
-@app.route("/feedback", methods=["GET", "POST"])
-def feedback():
+
+@app.route("/", methods=["GET", "POST"])
+def form():
     if request.method == "POST":
         name = request.form.get("username")
-        message = request.form.get("message")
-        
-        return render_template("thankyou.html", user=name, message=message)
-    
-    return render_template("feedback.html")
+        if not name:
+            flash("Name cannot be empty")
+            return redirect(url_for("form"))
+        flash(f"Thanks {name}, your feedback was saved.")
+        return render_template("thankyou.html", name=name)
+    return render_template("form.html")
+
+
+@app.route("/thankyou")
+def thankyou():
+    return render_template("thankyou.html")
+
